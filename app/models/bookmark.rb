6316d7adf6e221e000a53e2e49276e7bdc9055cc
benfrombooth <benjamin.schenkel@gmail.com>
2022-12-08 00:42:59 +0000

    Fixing up members/bookmarks

--- app/models/bookmark.rb
+++
@@ -1,5 +0,0 @@
-class Bookmark < ApplicationRecord
-  belongs_to(:politician, { :required => true, :class_name => "Politician", :foreign_key => "politician_id" })
-
-  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id" })
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_07_220129) do
+ActiveRecord::Schema.define(version: 2022_12_07_234639) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "politician_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "causes", force: :cascade do |t|
     t.integer "politician_id"
